864f333feaca08afcd3dd16dd6080f08992f59f0
HannahGeorge <75256187+HannahGeorge@users.noreply.github.com>
2021-06-29 18:25:21 +0530

    Update Umbrella.js

--- Umbrella.js
+++ Umbrella.js
@@ -10,7 +10,7 @@ class Umbrella {
         }
 
         this.body = Bodies.circle(x, y, 50, options);
-        this.image = loadImage("Walking Frame/walking_1.png");
+        this.image = loadImage("walking_1.png");
         World.add(world, this.body);
 
     }
